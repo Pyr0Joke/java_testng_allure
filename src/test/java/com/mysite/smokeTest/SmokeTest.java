bc0eda8ef6d3c7acbd87a3258131cc6c2cc969d9
Pyr0Joke <37038004+Pyr0Joke@users.noreply.github.com>
2018-10-05 11:54:42 +0300

    +testng xml

---
+++ src/test/java/com/mysite/smokeTest/SmokeTest.java
@@ -0,0 +1,20 @@
+package com.mysite.smokeTest;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+    @Test
+    public void s3() {
+        System.out.println("s3() - com.mysite.smokeTest.SmokeTest");
+    }
+
+    @Test
+    public void s1() {
+        System.out.println("s1() - com.mysite.smokeTest.SmokeTest");
+    }
+
+    @Test
+    public void s2() {
+        System.out.println("s2() - com.mysite.smokeTest.SmokeTest");
+    }
+}
--- src/test/java/mainTests.java
+++ src/test/java/mainTests.java
@@ -7,27 +7,28 @@ import ru.yandex.qatools.allure.annotations.Title;
 
 @Description("This is an example test suite")
 public class mainTests {
-    @Test
-    @Title("Successful test")
+
+    @Test(groups={"add"})
+    @Title("Good add")
     public void testGoodAdd(){
         Assert.assertEquals(5,2+3);
     }
 
-//    @Test
-//    @DisplayName("Bad Add")
-//    public void testBadAdd(){
-//        Assert.assertEquals(5,2+6);
-//    }
+    @Test(groups={"add"},dependsOnMethods={"testGoodAdd"})
+    @Title("Bad Add")
+    public void testBadAdd(){
+        Assert.assertEquals(5,2+6);
+    }
 
-    @Test
-    @Title("Successful test")
+    @Test(groups={"mult"})
+    @Title("Good mult")
     public void testGoodMult(){
         Assert.assertEquals(6,2*3);
     }
 
-//    @Test
-//    @Title("Failing test")
-//    public void testBadMult(){
-//        Assert.assertEquals(5,2*6);
-//    }
+    @Test(groups={"mult"})
+    @Title("Failing test")
+    public void testBadMult(){
+        Assert.assertEquals(5,2*6);
+    }
 }
